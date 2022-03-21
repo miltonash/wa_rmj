011f247ae3a3a32db9dedec4ce1bd80cac3bbee1
miltonash <Milton.Straw@grad.moore.sc.edu>
2022-03-21 15:05:21 -0400

    Twilio fix

--- code/send_sms.py
+++ code/send_sms.py
@@ -16,8 +16,12 @@ def send_sms():
     from twilio.rest import Client
 
     # the following line needs your Twilio Account SID and Auth Token
-    client = Client("AC10b7aceaf81589e8cf70ae2697c2ae61",
-                    "ca7ead1b352145c39911e3d2ef53cc2f")
+    sid = open('Account_SID.txt', 'r')
+    Account_SID = sid.read()
+    token = open('Auth_Token.txt', 'r')
+    Auth_Token = token.read()
+    client = Client(Account_SID,
+                    Auth_Token)
 
     # change the "from_" number to your Twilio number and the "to" number
     # to the phone number you signed up for Twilio with, or upgrade your
